e7acf39e2da46c6705a7fb51e39888a0195b077e
ChenYijunTCS <yijun.chen@tcs.com>
2026-06-12 13:46:18 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 def add(a, b, c=0):
-  try:
-    return int(a + b) + int(c)
-  except:
-    return "error"
+   if not isinstance(a, (int, float)):
+       return -1
+   if not isinstance(b, (int, float)):
+       return -1
+   if not isinstance(c, (int, float)):
+       return -1
+   if not (0 <= a <= 10):
+       return -2
+   if not (0 <= b <= 10):
+       return -2
+   if not (0 <= c <= 10):
+       return -2
+   return a + b + c
